8dd5a9059f4595007debdae1b3863d1d29907000
Kylan1940 <Kevingaming123copyright@gmail.com>
2026-07-25 10:56:13 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ const {
   Partials, 
   ActivityType
 } = require('discord.js');
-<<<<<<< HEAD
 const { Player } = require('discord-player');
 const { DefaultExtractors } = require('@discord-player/extractor');
-=======
->>>>>>> main
 
 const eventHandler = require('./handlers/eventHandler');
 
@@ -24,7 +21,6 @@ const client = new Client({
   partials: [Partials.Channel] 
 });
 
-<<<<<<< HEAD
 client.player = new Player(client);
 
 client.once('ready', async (c) => {
@@ -37,11 +33,6 @@ client.once('ready', async (c) => {
     console.error('❌ Failed to load extractors:', err.message);
   }
 
-=======
-client.once('clientReady', (c) => {
-  console.log(`✅ ${c.user.tag} is online.`);
-
->>>>>>> main
   client.user.setPresence({
     activities: [{
       name: '/help | kylan1940.netlify.app',
